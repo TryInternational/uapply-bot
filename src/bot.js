41856c5f743ec6f93a34c95a1806703702862300
syedaamish <aamish@try.city>
2023-05-24 17:10:31 +0530

    bugs fixes

--- src/bot.js
+++ src/bot.js
@@ -109,16 +109,18 @@ async function processAnswer(recipient, selectedOption) {
 }
 
 async function allQuestionsAnswered(recipient) {
+  console.log("innn");
   await client.sendMessage(
     recipient,
     "Thank you for answering all the questions!"
   );
+  console.log("ran");
 
   // Send a message to 8971640963
-  await client.sendMessage(
-    "8971640963",
-    `User ${recipient} has answered all the questions.`
-  );
+  //   await client.sendMessage(
+  //     "8971640963",
+  //     `User ${recipient} has answered all the questions.`
+  //   );
 
   closedUsers.push(recipient); // Add the recipient to the closedUsers array
 }
